0a3b41fee4b33c60443239527ea1e248144e3fff
ArthurOliveira147 <arthurbaschera15@gmail.com>
2024-08-20 20:07:55 -0300

    20/08

--- teste.c
+++ teste.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
-    printf(OLA);
+double Soma (double A, double B);
+
+int main(){
+
+    //operac˜oes matemáticas
+
+    printf (Escolha a operacao:);
+    printf (0. Sair);
+    printf (1. Soma);
+    printf (2. Subtracao);
+    printf (3. Multiplicacao);
+    printf (4. Divisao);
+    printf (5. Potenciacao);
+    printf (6. Raiz Quadrada);
+    printf (7. Fatorial);
+    printf (8. MDC);
+    printf (9. MMC);
+    printf (10. Equacao de Segundo Grau);
 
     return 0;
 }
