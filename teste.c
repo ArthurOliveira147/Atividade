ff10956dc3c31f25f2173c41b17a259f35c3e1b5
ArthurOliveira147 <arthurbaschera@gmail.com>
2024-08-27 20:47:42 -0300

    27/08

--- teste.c
+++ teste.c
@@ -4,6 +4,12 @@ double Soma (double A, double B);
 double Subtracao (double A, double B);
 double Multiplicacao (double A, double B);
 double Divisao (double A, double B);
+//double Potenciasao ();
+//double Raiz Quadrada ();
+//double Fatorial ();
+//int MDC ();
+//int MMC ();
+//double Equacao de Segundo Grau ();
 
 double A = 0.00000, B = 0.00000;
 int entrada = 0;
@@ -12,7 +18,7 @@ int main(){
 
     //menu
 
-    printf ("Escolha a operacao:\n");
+    printf ("Operacoes:\n");
     printf ("0. Sair\n");
     printf ("1. Soma\n");
     printf ("2. Subtracao\n");
@@ -24,53 +30,63 @@ int main(){
     printf ("8. MDC\n");
     printf ("9. MMC\n");
     printf ("10. Equacao de Segundo Grau\n");
-    printf ("Escolha uma operacao\n");
+    printf ("Escolha uma operacao:\n");
     scanf ("%d", &entrada);
-    
 
     switch (entrada){
       case 1:
         printf ("Digite o primeiro numero:\n");
-		scanf ("%d", &entrada);
+		scanf ("%lf", &A);
 		printf ("Digite o segundo numero:\n");
-		scanf ("%d", &entrada);
+		scanf ("%lf", &B);
+		printf ("resultado = %.5lf\n", Soma(A,B));
+	  case 2:
+	    printf ("Digite o primeiro numero:\n");
+	    scanf ("%lf", &A);
+	    printf ("Digite o segundo numero:\n");
+	    scanf ("%lf", &B);
+	    printf ("resultado = %.5lf\n", Subtracao(A,B));
+	break;
+	  case 3:
+	    printf ("Digite o primeiro numero:\n");
+	    scanf ("%lf", &A);
+	    printf ("Digite o segundo numero:\n");
+	    scanf ("%lf", &B);
+	    printf ("resultado = %.5lf\n", Multiplicacao(A,B));
+	break;
+	  case 4:
+	    printf ("Digite o primeiro numero:\n");
+	    scanf ("%lf", &A);
+	    printf ("Digite o segundo numero:\n");
+	    scanf ("%lf", &B);
+	    printf ("resultado = %.5lf\n", Divisao(A,B));
 	break;
     }
     
+     return 0;
+}   
     //calculos
+    
+double Soma (double A, double B){
+    double resultado = 0.00000;
+    resultado = A+B;
+    return (resultado);
+}    
 
-        double Soma (double A, double B){
-            double resultado = 0.00000;
-            scanf ("%lf\n%lf", &A, &B);
-            resultado = A+B;
-            printf ("resultado = %.5lf\n", resultado);
-            
-        return (resultado);
-    }    
-        double Subtracao (double A, double B){
-            double resultado = 0.00000;
-            scanf ("%lf\n%lf", &A, &B);
-            resultado = A-B;
-            printf ("resultado = %.5lf\n", resultado);
-            
-        return (resultado);
-    }
-        double Multiplicacao (double A, double B){
-            double resultado = 0.00000;
-            scanf ("%lf\n%lf", &A, &B);
-            resultado = A*B;
-            printf ("resultado = %.5lf\n", resultado);
-            
-        return (resultado);
-    }
-        double Divisao (double A, double B){
-            double resultado = 0.00000;
-            scanf ("%lf\n%lf", &A, &B);
-            resultado = A/B;
-            printf ("resultado = %.5lf\n", resultado);
-            
-        return (resultado);    
-        }
-        
-    return 0;
+double Subtracao (double A, double B){
+    double resultado = 0.00000;
+    resultado = A-B;
+    return (resultado);
+}
+    
+double Multiplicacao (double A, double B){
+    double resultado = 0.00000;
+    resultado = A*B;
+    return (resultado);
+}
+    
+double Divisao (double A, double B){
+    double resultado = 0.00000;
+    resultado = A/B;
+    return (resultado);    
 }
