73546e9322c85890e6ee1bb9703c2e1c806e4f38
ArthurOliveira147 <arthurbaschera15@gmail.com>
2024-10-30 22:19:13 -0300

    30/10

--- pgAdimin4 (Escola).sql
+++ pgAdimin4 (Escola).sql
@@ -4,9 +4,20 @@ CREATE TABLE disciplina(
 );
 
 CREATE TABLE professor(
+	telefone_professor int,
+	matricula_professor serial PRIMARY KEY,
 	cpf_professor bigint,
 	nome_professor varchar(20),
-	nascimento_professor date,
-	telefone_professor int,
-	matricula_professor serial
+	data_nac_professor date,
+	endereco_professor varchar(20)
 );
+
+DROP TABLE disciplina;
+
+SELECT * FROM disciplina ORDER BY codigo_disciplina;
+
+INSERT INTO disciplina (nome_disciplina) VALUES ('Ed.Fisica');
+INSERT INTO disciplina (nome_disciplina) VALUES ('Geografia');
+INSERT INTO disciplina (nome_disciplina) VALUES ('Inglês');
+INSERT INTO disciplina (nome_disciplina) VALUES ('Matematica');
+INSERT INTO disciplina (nome_disciplina) VALUES ('Portuges');
