dbeea2fd684d70e78e963ea628d0e7c9a3ff603b
ArthurOliveira147 <arthurbaschera15@gmail.com>
2024-11-06 22:14:21 -0300

    06/11

--- pgAdimin4 (Livraria).sql
+++ pgAdimin4 (Livraria).sql
@@ -48,32 +48,37 @@ SELECT * FROM clientes;
 SELECT * FROM vendas;
 SELECT * FROM itens_vendas;
 
-INSERT INTO autor (nome_autor, nascimento, nacionalidade) VALUES ('Machado de Assis','21/06/1839','Brasileiro');
-INSERT INTO autor (nome_autor, nascimento, nacionalidade) VALUES ('J.K.Rowling','31/07/1965','Inglesa');
-INSERT INTO autor (nome_autor, nascimento, nacionalidade) VALUES ('Dr.Seuss','02/03/1904','Estado Unidense');
-INSERT INTO autor (nome_autor, nascimento, nacionalidade) VALUES ('Monteiro Lobato','18/04/1882','Brasileiro');
-INSERT INTO autor (nome_autor, nascimento, nacionalidade) VALUES ('J.R.R. Tolkien','03/01/1892','Africano');
+INSERT INTO autor (nome_autor, nascimento, nacionalidade) VALUES
+('Machado de Assis','21/06/1839','Brasileiro'),
+('J.K.Rowling','31/07/1965','Britânica'),
+('Dr.Seuss','02/03/1904','Americano'),
+('Monteiro Lobato','18/04/1882','Brasileiro'),
+('J.R.R. Tolkien','03/01/1892','Africano'),
 
-INSERT INTO livros (titulo, publicacao, preco, estoque) VALUES ('O Alienista', '15/03/1882', 'R$ 31,40', '20');
-INSERT INTO livros (titulo, publicacao, preco, estoque) VALUES ('Harry Poter', '26/06/1997', 'R$ 55,74', '15');
-INSERT INTO livros (titulo, publicacao, preco, estoque) VALUES ('O Lorax', '23/06/1971', 'R$ 29,90', '15');
-INSERT INTO livros (titulo, publicacao, preco, estoque) VALUES ('O Saci', '1921', 'R$ 24,90', '20');
-INSERT INTO livros (titulo, publicacao, preco, estoque) VALUES ('Senhor dos Aneis', '29/07/1954', 'R$ 154,90', '10');
+INSERT INTO livros (titulo, publicacao, preco, estoque) VALUES
+('O Alienista', '15/03/1882', 'R$ 31,40', '20'),
+('Harry Poter', '26/06/1997', 'R$ 55,74', '15'),
+('O Lorax', '23/06/1971', 'R$ 29,90', '15'),
+('O Saci', '1921', 'R$ 24,90', '20'),
+('Senhor dos Aneis', '29/07/1954', 'R$ 154,90', '10'),
 
-INSERT INTO clientes (nome_cliente, email_cliente, telefone_cliente, data_cadastro) VALUES ('Kevin Guimaraes', 'kevin.guimaraes@gmail.com', '(24)4735-4385', '20/05/2001');
-INSERT INTO clientes (nome_cliente, email_cliente, telefone_cliente, data_cadastro) VALUES ('Arthur Oliveira', 'arthuroliveira15@gmail.com', '(32)4582-5475', '15/09/2009');
-INSERT INTO clientes (nome_cliente, email_cliente, telefone_cliente, data_cadastro) VALUES ('Vitoria Gabriela', 'gabriela.vitoria@gmail.com', '(22)4762-8543', '31/12/1999');
-INSERT INTO clientes (nome_cliente, email_cliente, telefone_cliente, data_cadastro) VALUES ('Carlos Alberto', 'CarlosAlberto@gmail.com', '(11)5549-4563', '05/11/1995');
-INSERT INTO clientes (nome_cliente, email_cliente, telefone_cliente, data_cadastro) VALUES ('Julia Mocelin', 'julia.mocelin@gmail.com', '(53)4275-4387', '01/01/2000');
+INSERT INTO clientes (nome_cliente, email_cliente, telefone_cliente, data_cadastro) VALUES
+('Kevin Guimaraes', 'kevin.guimaraes@gmail.com', '(24)4735-4385', '20/05/2001'),
+('Arthur Oliveira', 'arthuroliveira15@gmail.com', '(32)4582-5475', '15/09/2009'),
+('Vitoria Gabriela', 'gabriela.vitoria@gmail.com', '(22)4762-8543', '31/12/1999'),
+('Carlos Alberto', 'CarlosAlberto@gmail.com', '(11)5549-4563', '05/11/1995'),
+('Julia Mocelin', 'julia.mocelin@gmail.com', '(53)4275-4387', '01/01/2000'),
 
-INSERT INTO vendas (data_venda, valor_total) VALUES ('12/05/2010', 'R$ 500,00');
-INSERT INTO vendas (data_venda, valor_total) VALUES ('03/08/2024', 'R$ 500,00');
-INSERT INTO vendas (data_venda, valor_total) VALUES ('09/11/2007', 'R$ 500,00');
-INSERT INTO vendas (data_venda, valor_total) VALUES ('14/01/2005', 'R$ 500,00');
-INSERT INTO vendas (data_venda, valor_total) VALUES ('30/10/2020', 'R$ 500,00');
+INSERT INTO vendas (data_venda, valor_total) VALUES
+('12/05/2010', 'R$ 500,00'),
+('03/08/2024', 'R$ 500,00'),
+('09/11/2007', 'R$ 500,00'),
+('14/01/2005', 'R$ 500,00'),
+('30/10/2020', 'R$ 500,00'),
 
-INSERT INTO itens_vendas (quantidade, preco_unitario) VALUES ('', 'R$ ');
-INSERT INTO itens_vendas (quantidade, preco_unitario) VALUES ('', 'R$ ');
-INSERT INTO itens_vendas (quantidade, preco_unitario) VALUES ('', 'R$ ');
-INSERT INTO itens_vendas (quantidade, preco_unitario) VALUES ('', 'R$ ');
-INSERT INTO itens_vendas (quantidade, preco_unitario) VALUES ('', 'R$ ');
+INSERT INTO itens_vendas (quantidade, preco_unitario) VALUES
+('', 'R$ '),
+('', 'R$ '),
+('', 'R$ '),
+('', 'R$ '),
+('', 'R$ '),
